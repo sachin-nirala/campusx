bc70f8b3e87be626856e17ea56f0b788f8c52d77
sachin-nirala <sachinnirala702@gmail.com>
2024-12-03 17:31:54 +0530

    second code program

---
+++ campusx question/q1 To q10.py
@@ -0,0 +1,10 @@
+Question :( 1.)--------------------------------------------------------------------------------------------------------------------------------------------------
+n1 = int(input("Enter 1st number:-  "))
+n2 = int(input("Enter 2nd number:-  "))
+n3 = int(input("Enter 3rd number:-  "))
+if n1 > n2 and n1 > n3:
+    print(n1, " is greastest")
+elif n2 > n1 and n2 > n3:
+    print(n2, " is greatest.")
+elif n3 > n1 and n3 > n2:
+    print(n3, "is greatest.")
