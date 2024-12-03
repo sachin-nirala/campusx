414abbb3f286ed2265bb1cdedfa628a482db676a
sachin-nirala <sachinnirala702@gmail.com>
2024-12-03 18:56:06 +0530

    Question (10 added).

--- campusx question/q10Toq20.py
+++ campusx question/q10Toq20.py
@@ -1,1 +1,9 @@
-print("this is question 10")
+# Question:- (10)--------------------------------------------
+# Write a program that will take user input of cost price and selling price and determines whether its a loss or a profit.
+c_price = float(input("chal product ka cost price bta."))
+s_price = float(input(" And selling price bhi bata."))
+revenue_cost = s_price - c_price
+if revenue_cost < 0:
+    print(f"you lost {revenue_cost}rs")
+else:
+    print(f"you are in profit and your profit is {revenue_cost}rs")
