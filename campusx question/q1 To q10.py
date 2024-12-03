4fe1aa15c0d808411f1c2986a1f66c909bbc07a0
sachin-nirala <sachinnirala702@gmail.com>
2024-12-03 18:19:12 +0530

    Your commit message

--- campusx question/q1 To q10.py
+++ campusx question/q1 To q10.py
@@ -1,4 +1,4 @@
-Question :( 1.)--------------------------------------------------------------------------------------------------------------------------------------------------
+# Question :( 1.)--------------------------------------------------------------------------------------------------------------------------------------------------
 n1 = int(input("Enter 1st number:-  "))
 n2 = int(input("Enter 2nd number:-  "))
 n3 = int(input("Enter 3rd number:-  "))
@@ -8,3 +8,14 @@ elif n2 > n1 and n2 > n3:
     print(n2, " is greatest.")
 elif n3 > n1 and n3 > n2:
     print(n3, "is greatest.")
+
+
+# Question:--(2)-------------------------------------------------------------------------------------------------------------------
+n1 = int(input("Enter 1st number:-  "))
+temp = n1
+n2 = int(input("Enter 2nd number:-  "))
+print( "first", n1, "second", n2)
+n1 = n2
+n2 = temp
+
+print("updated numbers n1 and n2 are", n1,n2)
