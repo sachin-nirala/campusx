547ced37710777c5add75f402bbb1b8e1141ae39
sachin-nirala <sachinnirala702@gmail.com>
2024-12-10 19:00:26 +0530

    New questions added

--- campusx question/q20To30.py
+++ campusx question/q20To30.py
@@ -1,1 +1,54 @@
-print("This is question 30.")
+print("This is question 3nnm0.")
+#question(24.)Write a program to find the sum of first n numbers, where n will be provided by the user. Eg if the user provides n=10 the output should be 55.
+# num = int(input("Enter a number:- "))
+# result = 0
+# for i in range(1,num+1):
+#     result = result + i
+# print(result)
+
+
+#queation (23.)-----------------------------------------------------------     
+    #23. Write a program that will swap number
+a = 10 
+b = 20
+
+# print("previous A and B", a, b)
+# a,b = b,a
+# print('after swap A and B', a,b)
+
+# using third valiable
+
+# print("previous A and B", a, b)
+# temp = a 
+# a = b 
+# b = a
+# print('after swap A and B',a,b)
+
+
+#question(25.) ----------------------------------------------------------------------
+# Write a program that can multiply 2 numbers provided by the user without using the * operator
+
+# x = 23
+# b = 2
+# print(2**3)
+#question(27)-------------------------------------------
+# Write a program to print the first 25 odd numbers
+count = 0
+for i in range(1,10000):
+    if i % 2 != 0:
+    # count = count + 1
+        print(i)
+        count = count + 1
+        if count == 25:
+            break
+            
+    
+print("counting of 'C'",count)
+
+for i in range (1,50,2):
+    print(i)
+
+
+
+
+
